b005d87bf7314450a86346b3487bc4e93855600e
Krunal Hedaoo <37552920+djinn09@users.noreply.github.com>
2026-05-08 23:34:12 +0530

    refactor(tokenizer): rename sentencepiece module to sentence_piece
    Rename `sentencepiece.rs` to `sentence_piece.rs` and implement the `SentencePieceTokenizer` with seed vocabulary building logic. This update aligns the file naming with Rust's snake_case conventions and provides a more functional boilerplate for the tokenizer.

---
+++ sentence_piece.rs
@@ -0,0 +1,120 @@
+mod tokenizer;
+use crate::tokenizer::Tokenizer;
+use std::{collections::HashMap, iter};
+
+pub struct SentencePieceTokenizer {
+    vocab: HashMap<String, f64>,         // token-> log_prob
+    token_to_id: HashMap<String, usize>, // token-> id
+    id_to_token: Vec<String>,            // id-> token
+}
+
+impl SentencePieceTokenizer {
+    const MAX_SUBSTR_LEN: usize = 16;
+    const SEED_VOCAB_MULTIPLIER: usize = 16;
+
+    pub fn new() -> Self {
+        Self {
+            vocab: HashMap::new(),
+            token_to_id: HashMap::new(),
+            id_to_token: Vec::new(),
+        }
+    }
+
+    fn build_seed_vocab(
+        &mut self,
+        text: &str,
+        max_sub_str_len: usize,
+        seed_vocab_size: usize,
+    ) -> HashMap<String, f64> {
+        // _marked word boundary: hello world -> _hello_world
+
+        let marked_text = text
+            .split_whitespace()
+            .map(|w| format!("_{w}"))
+            .collect::<Vec<_>>()
+            .join("");
+
+        // substring count upto max substing length
+        let chars: Vec<char> = marked_text.chars().collect();
+        let mut counts: HashMap<String, usize> = HashMap::new();
+        for i in 0..chars.len() {
+            for j in 1..max_sub_str_len.min(chars.len() - i) {
+                let substr: String = chars[i..i + j].iter().collect();
+                *counts.entry(substr).or_insert(0) += 1;
+            }
+        }
+
+        //single characters alwasys in vocab...
+        let single_chr: Vec<(String, usize)> = counts
+            .iter()
+            .filter(|(k, _)| k.chars().count() == 1)
+            .map(|(k, v)| (k.clone(), *v))
+            .collect();
+        // multiple character and sort with top frequecny
+        let mut mult_substr: Vec<(String, usize)> = counts
+            .iter()
+            .filter(|(k, _)| k.chars().count() > 1)
+            .map(|(k, v)| (k.clone(), *v))
+            .collect();
+        // sort by frequency in descending order
+        mult_substr.sort_by(|a, b| b.1.cmp(&a.1));
+
+        //init seed vocab with all single characters
+        let mut seed: HashMap<String, f64> = HashMap::new();
+        for (t, c) in single_chr {
+            seed.insert(t, c as f64);
+        }
+        // takeing remaining size of vocab from mult_substr sorted by frequency
+        let remaining_size = seed_vocab_size - seed.len();
+        for (t, c) in mult_substr.iter().take(remaining_size) {
+            seed.insert(t.clone(), *c as f64);
+        }
+        let log_total_tokens: f64 = seed.values().sum();
+
+        seed.into_iter()
+            .map(|(k, v)| (k, (v / log_total_tokens).ln()))
+            .collect()
+    }
+
+    pub fn train(&mut self, text: &str, vocab_size: usize, _allowed_special: Option<Vec<String>>) {
+        self.vocab = self.build_seed_vocab(
+            text,
+            Self::MAX_SUBSTR_LEN,
+            vocab_size * Self::SEED_VOCAB_MULTIPLIER,
+        );
+        // self.save_vocab("./seed_vocab.txt").unwrap();
+        // println!("Saved vocab to seed_vocab.txt");
+        println!("Seed vocab size: {}", self.vocab.len());
+    }
+    // fn save_vocab(&self, path: &str) -> Result<(), String> {
+    //     let mut tokens: Vec<(&String, &f64)> = self.vocab.iter().collect();
+    //     tokens.sort_by(|a, b| b.1.partial_cmp(a.1).unwrap()); // highest log-prob first
+
+    //     let lines: Vec<String> = tokens
+    //         .iter()
+    //         .map(|(tok, lp)| format!("{}\t{:.6}", tok, lp))
+    //         .collect();
+
+    //     std::fs::write(path, lines.join("\n")).map_err(|e| e.to_string())
+    // }
+}
+
+impl Tokenizer for SentencePieceTokenizer {
+    fn encode(&mut self, _text: &str) -> Result<Vec<usize>, String> {
+        unimplemented!("SentencePiece encode is intentionally left as boilerplate");
+    }
+
+    fn decode(&self, _ids: &[usize]) -> Result<String, String> {
+        unimplemented!("SentencePiece decode is intentionally left as boilerplate");
+    }
+}
+
+fn main() {
+    let text =
+        std::fs::read_to_string("./the-verdict.txt").expect("Expect file but failed to read");
+
+    println!("Read {} characters from the file.", text.len());
+
+    let mut tokenizer = SentencePieceTokenizer::new();
+    tokenizer.train(&text, 500, None);
+}
